07156d68fa9dda4ff51b79889ccc96f41e59ae2b
hk2511 <strapkobrest@gmail.com>
2018-12-03 19:10:26 +0300

    1

---
+++ find_nimber/1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    srand(static_cast<unsigned int>(time(0)));
+    intsecretNumber = rand() % 100 + 1;
+    int tries = 0;
+    int guess;
+    cout<< "\twelcome to Guess my number\n\n";
+
+    do 
+    {
+        cout<< "Enter a guess: ";
+        cin >> guess:
+        ++tries;
+        if(guess > secretNumber)
+        {
+            cout<< "Too high\n\n";
+        }
+        else if (guess < secretNumber)
+        {
+            cout << "Too low\n\n";
+        }   
+        else 
+        {
+            cout << "\nThat's is you number" << tries << "exelent\n";
+        }
+    }while (guess != secretNumber);
+
+    return 0;
+}
