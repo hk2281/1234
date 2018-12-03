810874ee1eac969192fcc40d92d90e33beaa77b5
hk2511 <strapkobrest@gmail.com>
2018-12-03 18:09:57 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int x,y;
+    cin >> x >> y>> endl;
+    cout << x;
+    cout << y;
+
+    return 0;
+}
+for (init; condition; increment){
+    statemen(s);
+}
