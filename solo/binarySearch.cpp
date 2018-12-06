c05425cda1241814fc71443ad1b15b720c791c2e
hk2511 <strapkobrest@gmail.com>
2018-12-06 10:54:52 +0300

    1

--- solo/binarySearch.cpp
+++ solo/binarySearch.cpp
@@ -7,14 +7,20 @@ using namespace std;
 int main()
 {   
     int unkndownNum, startGap, finishGap, startPoint;
+    cout << "enter u secret number: " ; 
+    cin >> unkndownNum; 
     cout << "enter range\nstart point  of range: " ; 
     cin >> startGap;
     cout << "finish point  of range: " ;
     cin >> finishGap;
     startPoint = ((finishGap-startGap)/2)+startGap;
     cout << " start point: "<<startPoint<< endl;
-    
-    
+
+    do{
+        cout << "u number out of renge: ";
+        cin >> unkndownNum;
+    }while (unkndownNum < startGap || unkndownNum > finishGap);
+    // нахождение точки входа 
     if(unkndownNum<startGap)
     {
         startPoint += ((startPoint-startGap)/2);
@@ -22,5 +28,5 @@ int main()
     }
 
 
-    return 0;
+    
 }
